409f44554a4b805a578b0d3067434d9d60b2bcd6
nickolation <sf.1951@mail.ru>
2023-07-04 12:13:05 +0300

    fix mask: remove dedudant parts, expand test cases

--- flatmask_test.go
+++ flatmask_test.go
@@ -1,7 +1,6 @@
 package flatmask
 
 import (
-	"strings"
 	"testing"
 
 	"google.golang.org/protobuf/types/known/fieldmaskpb"
@@ -28,17 +27,7 @@ func (m *maskTable) GetPaths() []string {
 	return m.p
 }
 
-func (m *maskTable) Reset() {
-}
-
-func (m *maskTable) Normalize() {
-}
-
-func (m *maskTable) String() string {
-	return strings.Join(m.p, ".")
-}
-
-func TestReduceFlatMasks(t *testing.T) {
+func TestReduceFlatMasksChild(t *testing.T) {
 	table := []*maskTable{
 		{
 			p:        []string{"a.b.c"},
@@ -63,7 +52,6 @@ func TestReduceFlatMasks(t *testing.T) {
 			)
 		}
 	}
-
 }
 
 type fieldmaskTable struct {
@@ -71,7 +59,7 @@ type fieldmaskTable struct {
 	expected []string
 }
 
-func TestReduceFieldmasks(t *testing.T) {
+func TestReduceFieldmasksChild(t *testing.T) {
 	table := []fieldmaskTable{
 		{
 			mask: &fieldmaskpb.FieldMask{
@@ -103,3 +91,118 @@ func TestReduceFieldmasks(t *testing.T) {
 		}
 	}
 }
+
+func TestReduceFieldmasksZero(t *testing.T) {
+	table := []fieldmaskTable{
+		{
+			mask: &fieldmaskpb.FieldMask{
+				Paths: []string{"a.b.c", "a.b.c.d", "g.awa"},
+			},
+			expected: []string{},
+		},
+	}
+
+	for _, tt := range table {
+		rm := Reduce(tt.mask, 0)
+		if !testEq(rm.GetPaths(), tt.expected) {
+			t.Errorf(
+				"Reduce(%v): actual - %v, exptected - %v",
+				tt.mask.GetPaths(), rm.GetPaths(), tt.expected,
+			)
+		}
+	}
+}
+
+func TestReduceFieldmasksTotal(t *testing.T) {
+	table := []fieldmaskTable{
+		{
+			mask: &fieldmaskpb.FieldMask{
+				Paths: []string{"a.b.c", "a.b.c.d", "g.awa"},
+			},
+			expected: []string{"a", "g"},
+		},
+	}
+
+	for _, tt := range table {
+		rm := Reduce(tt.mask, Total)
+		if !testEq(rm.GetPaths(), tt.expected) {
+			t.Errorf(
+				"Reduce(%v): actual - %v, exptected - %v",
+				tt.mask.GetPaths(), rm.GetPaths(), tt.expected,
+			)
+		}
+	}
+}
+
+func TestReduceFieldmasksChildAfterRoot(t *testing.T) {
+	table := []fieldmaskTable{
+		{
+			mask: &fieldmaskpb.FieldMask{
+				Paths: []string{"a.b.c", "a.b.c.d", "g.awa"},
+			},
+			expected: []string{"a.b", "g.awa"},
+		},
+		{
+			mask: &fieldmaskpb.FieldMask{
+				Paths: []string{"1.12.512", "1", "2.52", "2.81", "2.52.3"},
+			},
+			expected: []string{"1", "1.12", "2.52", "2.81"},
+		},
+	}
+
+	for _, tt := range table {
+		_ = Reduce(tt.mask, Total)
+		rm := Reduce(tt.mask, RootChild)
+
+		if !testEq(rm.GetPaths(), tt.expected) {
+			t.Errorf(
+				"Reduce(%v): actual - %v, exptected - %v",
+				tt.mask.GetPaths(), rm.GetPaths(), tt.expected,
+			)
+		}
+	}
+}
+
+func TestReduceFieldmasksSecondChild(t *testing.T) {
+	table := []fieldmaskTable{
+		{
+			mask: &fieldmaskpb.FieldMask{
+				Paths: []string{"a.b.c.d.e", "a.b.c.d", "g.a", "g.a.c", "g.a.c.1"},
+			},
+			expected: []string{"a.b.c", "g.a", "g.a.c"},
+		},
+	}
+
+	for _, tt := range table {
+		rm := Reduce(tt.mask, 3)
+
+		if !testEq(rm.GetPaths(), tt.expected) {
+			t.Errorf(
+				"Reduce(%v): actual - %v, exptected - %v",
+				tt.mask.GetPaths(), rm.GetPaths(), tt.expected,
+			)
+		}
+	}
+}
+
+func TestReduceFieldmasksNil(t *testing.T) {
+	table := []fieldmaskTable{
+		{
+			mask:     nil,
+			expected: []string{},
+		},
+	}
+
+	for _, tt := range table {
+		rm := Reduce(tt.mask, 3)
+
+		if !testEq(rm.GetPaths(), tt.expected) {
+			t.Errorf(
+				"Reduce(%v): actual - %v, exptected - %v",
+				tt.mask.GetPaths(), rm.GetPaths(), tt.expected,
+			)
+		}
+	}
+}
+
+
